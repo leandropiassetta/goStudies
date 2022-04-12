5a831c1cadc318314aa5354aabd299eb544b8b1f
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-04-12 14:23:54 -0300

    Primeiro programa feito em GOlang

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,9 @@
+package main // pacote principal do programa é o main por que esse programa que irá ser executado
+
+import "fmt" // import de outro um outro pacote de um core da linguagem
+
+// função main que é a porta de entrada de um programa GO
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
