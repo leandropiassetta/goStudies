ac42a810d747338b1958c751257667199bf52bac
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-04-29 16:17:45 -0300

    Operadores unários

---
+++ fundamentos/unario/unario.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	y := 2
+
+	// apenas postfix
+	x++ // x += 1 ou x = x + 1
+	fmt.Println(x)
+
+	y-- // y -= 1 ouy = y -1
+	fmt.Println(y)
+
+	// fmt.Println(x == y---) ----> A linguagem GO nao aceita esse tipo de comparação
+}
