ed6e96387a64d354d527f45b2f22339b381638e0
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-05-02 17:07:36 -0300

    switch 2

--- controles/switch2/switch.go
+++ controles/switch2/switch.go
@@ -5,6 +5,27 @@ import (
 	"time"
 )
 
+// exercicio:
+
+func notaParaConceito(n float64) string {
+	nota := int(n)
+	switch {
+
+	case nota > 9 && nota <= 10:
+		return "A"
+	case nota >= 8 && nota < 9:
+		return "B"
+	case nota >= 5 && nota < 8:
+		return "C"
+	case nota >= 2 && nota < 5:
+		return "D"
+	case nota >= 0 && nota < 2:
+		return "E"
+	default:
+		return "Nota inválida"
+	}
+}
+
 func main() {
 	t := time.Now()     // hora atual
 	switch /* true */ { // switch true --> Se não colocar o switch vinculado a algum caso, ele vai procurar algum case, que seja verdadeiro, ai irei botar alguns cases com verdadeiros ou falso e o primeiro verdadeiro que ele encontrar, ele vai executar..
@@ -15,4 +36,11 @@ func main() {
 	default:
 		fmt.Println("Boa noite.")
 	}
+
+	fmt.Println(notaParaConceito(-9))
+	fmt.Println(notaParaConceito(8.9))
+	fmt.Println(notaParaConceito(7))
+	fmt.Println(notaParaConceito(5))
+	fmt.Println(notaParaConceito(2))
+	fmt.Println(notaParaConceito(0))
 }
