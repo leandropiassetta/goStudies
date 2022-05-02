f084d5d44d6fc53eb3f270b8f9e48e0c24b8ee17
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-05-02 18:30:41 -0300

    switch3

---
+++ controles/switch3/switch.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// interface -> tipo genérico
+func tipo(i interface{}) string {
+	// sintaxe pra esse tipo de switch
+	switch i.(type) {
+	case int:
+		return "inteiro"
+	case float32, float64:
+		return "real"
+	case string:
+		return "string"
+	case func():
+		return "função"
+	default:
+		return "não sei"
+	}
+}
+
+func main() {
+	fmt.Println(tipo(2.3))
+	fmt.Println(tipo(1))
+	fmt.Println(tipo("Opa"))
+	fmt.Println(tipo(func() {}))
+	fmt.Println(tipo(time.Now()))
+}
