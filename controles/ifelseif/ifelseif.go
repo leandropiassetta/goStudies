8686e4e373b3423122401a9f5165215d696adf3f
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-05-02 13:21:52 -0300

    if/Else if

---
+++ controles/ifelseif/ifelseif.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(n float64) string {
+	if n >= 9 && n <= 10 {
+		return "A"
+	} else if n >= 8 && n < 9 {
+		return "B"
+	} else if n >= 5 && n < 8 {
+		return "C"
+	} else if n >= 3 && n < 5 {
+		return "D"
+	} else {
+		return "E"
+	}
+}
+
+// SWITCH -> desenhado para ter multiplas seleções
+func main() {
+	fmt.Println(notaParaConceito(9.9))
+	fmt.Println(notaParaConceito(8.9))
+	fmt.Println(notaParaConceito(6.9))
+	fmt.Println(notaParaConceito(3.9))
+	fmt.Println(notaParaConceito(2.9))
+}
