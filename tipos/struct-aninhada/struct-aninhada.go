cac50458b489e573851aa0c13ff7e63f6b17321c
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-05-05 15:25:03 -0300

    struct aninhada

--- tipos/struct-aninhada/struct-aninhada.go
+++ tipos/struct-aninhada/struct-aninhada.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Aquela estrutura básica de um pedido de venda, você tem o pedido e os itens do pedido...
 type item struct {
 	produtoID  int
@@ -9,8 +11,27 @@ type item struct {
 
 type pedido struct {
 	userID int
-	itens  []item // slice de item
+	// struct aninhado
+	itens []item // slice de item
+}
+
+func (p pedido) valorTotal() float64 {
+	total := 0.0
+
+	for _, item := range p.itens {
+		total += item.preco * float64(item.quantidade)
+	}
+	return total
 }
 
 func main() {
+	pedido := pedido{
+		userID: 1,
+		itens: []item{
+			{produtoID: 1, quantidade: 2, preco: 12.10}, // seguir esse tipo de padrão, mais fácil de identificar o que é cada valor no meu slice.
+			{2, 1, 23.49},
+			{3, 100, 3.13},
+		},
+	}
+	fmt.Printf("Valor total do pedido é R$ %.2f", pedido.valorTotal())
 }
