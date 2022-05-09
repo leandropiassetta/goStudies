4ce53d3a1d87e735371b986bf94ea253b2a4a02f
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-05-09 18:17:08 -0300

    criando & instalando um pacote do github

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/leandropiassetta/go-studies
 
 go 1.18
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/leandropiassetta/area-func v0.0.0-20220509192424-6a75c82a7063 // indirect
+	github.com/leandropiassetta/goarea v0.0.0-20220509204935-91723b1e56aa // indirect
+	github.com/vinigofr/go-package-publish v0.0.0-20220509190743-0d05b232a16f // indirect
+)
--- pacotes/reuso/main.go
+++ pacotes/reuso/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/leandropiassetta/area-func"
+)
 
 func main() {
-	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Circ(4.0))
 	fmt.Println(area.Rect(5.0, 2.0))
 	// Essa função não existe, nao foi definida, ela foi definida de forma privada, dentro de um pacote area, se eu estou em outro pacote não consigo utilizar essa função.
-	fmt.Println(area._TrianguloEq(5.0, 2.0))
+	// fmt.Println(area._TrianguloEq(5.0, 3.0))
 }
