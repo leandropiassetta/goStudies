cb568cbf909315b191ce589cb9c6af58691e034b
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-05-13 17:41:55 -0300

    executando inserts

--- sql/inserts/insert.go
+++ sql/inserts/insert.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:*senha@/goStudies") // obs: conecto com o banco que eu criei antes
+	db, err := sql.Open("mysql", "root:*Tigre91@/goStudies") // obs: conecto com o banco que eu criei antes
 	if err != nil {
 		panic(err)
 	}
@@ -17,8 +17,8 @@ func main() {
 	// statement = stmt
 
 	stmt, _ := db.Prepare("insert into usuarios(nome) values(?)")
-	stmt.Exec("Maria") // quando eu for executar esses statement, ai eu passo o valor que eu quero que ele substitua nessa interrogação;
-	stmt.Exec("João")  // função Exec retorna uma resposta, inclusive nessa resposta, vem o id do item que acabou de ser inserido no banco de dados.
+	stmt.Exec("Maria jose") // quando eu for executar esses statement, ai eu passo o valor que eu quero que ele substitua nessa interrogação;
+	stmt.Exec("João")       // função Exec retorna uma resposta, inclusive nessa resposta, vem o id do item que acabou de ser inserido no banco de dados.
 
 	res, _ := stmt.Exec("Pedro")
 
