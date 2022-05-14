a37c75dda4d2a5ed477987d6b0ec8d2ef2b4680b
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-05-14 14:43:22 -0300

    Executando Upodate e Delete

---
+++ sql/update/update.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:*Tigre91@/goStudies")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	stmt, _ := db.Prepare("update usuarios set nome = ? where id = ?")
+
+	stmt.Exec("Uóxiton Istive", 1)
+	stmt.Exec("Sheriston Uasleska", 2)
+
+	stmt2, _ := db.Prepare("delete from usuarios where id = ?")
+	stmt2.Exec(3)
+}
