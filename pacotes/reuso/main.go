9a2c02cd576bbc308de6ee58426d9dd05997b324
leandropiassetta <leandropiassetta@gmail.com>
2022-07-14 14:43:20 -0300

    test

--- pacotes/reuso/main.go
+++ pacotes/reuso/main.go
@@ -10,5 +10,5 @@ func main() {
 	fmt.Println(area.Circ(4.0))
 	fmt.Println(area.Rect(5.0, 2.0))
 	// Essa função não existe, nao foi definida, ela foi definida de forma privada, dentro de um pacote area, se eu estou em outro pacote não consigo utilizar essa função.
-	// fmt.Println(area._TrianguloEq(5.0, 3.0))
+	// fmt.Println(area._TrianguloEq(5.0, 3.0)) .."
 }
